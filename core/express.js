08dbe2a2b453d9f0d60099bc287b7cd4f4effd23
Ioan CHIRIAC <i.chiriac@yahoo.com>
2014-04-21 15:10:25 +0200

    replace with nodejs scripts

---
+++ core/express.js
@@ -0,0 +1,55 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var swig = require('swig');
+var config = require('../config/config');
+
+module.exports = function(app) {
+
+    console.log('... configuring express');
+
+    app.set('showStackError', config.showStackError);
+
+    //Prettify HTML
+    app.locals.pretty = config.prettyHTML;
+
+    //Should be placed before express.static
+    app.use(require('compression')({
+        filter: function(req, res) {
+            return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
+        },
+        level: 9
+    }));
+
+    //Setting the fav icon and static folder
+    app.use(express.static(config.root + '/public'));
+
+    //Set views path, template engine and default layout
+    swig.setDefaults({ cache: false });
+    app.engine('html', swig.renderFile);
+    app.set('view engine', 'html');
+    app.set('views', config.root + '/app/views');
+
+    //Enable jsonp
+    app.enable("jsonp callback");
+
+    //cookieParser should be above session
+    app.use(require('cookie-parser')());
+
+    // request body parsing middleware should be above methodOverride
+    app.use(require('body-parser')());
+    app.use(require('method-override')());
+
+    //express/mongo session storage
+    app.use(require('express-session')({
+        secret: '$uper$ecret$e$$ionKey'
+    }));
+
+    //connect flash for flash messages
+    app.use(require('connect-flash')());
+
+    //dynamic helpers
+    app.use(require('view-helpers')(config.app.name));
+
+};
---
+++ core/routes.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+var express = require('express');
+
+// Application routing
+module.exports = function(app) {
+    console.log('... configuring routes');
+
+    //routes should be at the last
+    var router = express.Router();
+    var path = app.config.root + '/app/controllers';
+    var files = fs.readdirSync(path);
+    for(var i in files) {
+        var controller = require(path + '/' + files[i]);
+        for(var a in controller) {
+            var action = controller[a];
+            if (action.hasOwnProperty('path')) {
+                if (action.hasOwnProperty('params')) {
+                    for(var i in action.params) {
+                        router.param(i, action.params[i]);
+                    }
+                }
+                var route = router.route(action.path);
+                if (action.hasOwnProperty('all')) route.all(action.all);
+                if (action.hasOwnProperty('get')) route.get(action.get);
+                if (action.hasOwnProperty('put')) route.all(action.put);
+                if (action.hasOwnProperty('post')) route.all(action.post);
+                if (action.hasOwnProperty('delete')) route['delete'](action['delete']);
+            }
+        }
+    }
+    app.use(router);
+    //Assume "not found" in the error msgs is a 404. this is somewhat silly, but valid, you can do whatever you like, set properties, use instanceof etc.
+    app.use(function(err, req, res, next) {
+        //Treat as 404
+        if (~err.message.indexOf('not found')) return next();
+
+        //Log it
+        console.error(err.stack);
+
+        //Error page
+        res.status(500).render('500', {
+            error: err.stack
+        });
+    });
+
+    //Assume 404 since no middleware responded
+    app.use(function(req, res, next) {
+        res.status(404).render('404', {
+            url: req.originalUrl,
+            error: 'Not found'
+        });
+    });
+};
