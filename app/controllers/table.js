346e6c9c364850166a757a95c6baea10e2838615
Ioan CHIRIAC <i.chiriac@yahoo.com>
2014-04-21 21:16:19 +0200

    start a new screen

---
+++ app/controllers/table.js
@@ -0,0 +1,16 @@
+/**
+ * The tables CRUD manager
+ */
+module.exports = {
+  list: {
+    params: {
+      'name': /^[a-zA-Z0-9_]+$/
+    },
+    path: '/table/:name',
+    get: function(req, res) {
+      res.render('table/list', {
+        table: req.params.name
+      });
+    }
+  }
+};
